81b39874f32e5cdf27a0ae250807612fe9a9ef6a
Hashir <hashirbinatif83@gmail.com>
2025-11-05 18:02:21 +0500

    second commit

---
+++ main.js
@@ -0,0 +1,11 @@
+const nav = document.querySelector('nav');
+
+window.addEventListener('scroll', () => {
+    if(window.scrollY > 60){
+        nav.classList.add("scrolled")
+    }
+
+    else{
+        nav.classList.remove("scrolled");
+    }
+})
